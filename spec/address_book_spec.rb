a27cdf91df0baf533092bbcb78ce4d9ba8751d21
Hannah Jeror <jerorhn@yahoo.com>
2018-03-01 15:38:39 -0500

    Add ability to import csv data

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,18 +1,22 @@
 require_relative '../models/address_book'
 
 RSpec.describe AddressBook do
+  let(:book) {AddressBook.new}
+
+  def check_entry(entry, expected_name, expected_number, expected_email)
+    expect(entry.name).to eq expected_name
+    expect(entry.phone_number).to eq expected_number
+    expect(entry.email).to eq expected_email
+  end
 
   describe "attributes" do
     it "responds to entries" do
-      book = AddressBook.new
       expect(book).to respond_to(:entries)
     end
     it "initializes entries as an array" do
-      book = AddressBook.new
       expect(book.entries).to be_an(Array)
     end
     it "initializes entries as empty" do
-      book = AddressBook.new
       expect(book.entries.size).to eq(0)
     end
   end
@@ -20,12 +24,10 @@ RSpec.describe AddressBook do
 
   describe "#add_entry" do
     it "adds only one entry to the address book" do
-      book = AddressBook.new
       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       expect(book.entries.size).to eq(1)
     end
     it "adds the correct information to entries" do
-      book = AddressBook.new
       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       new_entry = book.entries[0]
 
@@ -38,14 +40,12 @@ RSpec.describe AddressBook do
 
   describe "#remove_entry" do
     it "removes only one entry to the address book" do
-      book = AddressBook.new
       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       book.add_entry('Beth Lovelace', '010.483.1815', 'beth.king@lovelace.com')
       book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       expect(book.entries.size).to eq(1)
     end
     it "removes the correct information from entries when the first entry is removed" do
-      book = AddressBook.new
       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       book.add_entry('Beth Lovelace', '010.483.1815', 'beth.king@lovelace.com')
       book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
@@ -55,7 +55,6 @@ RSpec.describe AddressBook do
       expect(book.entries[0].email).to eq('beth.king@lovelace.com')
     end
     it "removes the correct information from entries when a later entry is removed" do
-      book = AddressBook.new
       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       book.add_entry('Beth Lovelace', '010.483.1815', 'beth.king@lovelace.com')
       book.add_entry('Dolly Lovelace', '483.483.1815', 'dolly.king@lovelace.com')
@@ -70,5 +69,38 @@ RSpec.describe AddressBook do
       expect(book.entries[1].email).to eq('dolly.king@lovelace.com')
     end
   end
+  describe "#import_from_csv" do
+    it "imports the correct number of entries" do
+      book.import_from_csv("entries.csv")
+      book_size = book.entries.size
 
+      expect(book_size).to eq 5
+    end
+    it "imports the 1st entry" do
+      book.import_from_csv("entries.csv")
+      entry_one = book.entries[0]
+      check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+    end
+    it "imports the 2nd entry" do
+      book.import_from_csv("entries.csv")
+      entry_two = book.entries[1]
+      check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+    end
+    it "imports the 3rd entry" do
+      book.import_from_csv("entries.csv")
+      entry_three = book.entries[2]
+      check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+    end
+    it "imports the 4th entry" do
+      book.import_from_csv("entries.csv")
+      entry_four = book.entries[3]
+      check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
+    end
+    it "imports the 5th entry" do
+      book.import_from_csv("entries.csv")
+      entry_five = book.entries[4]
+      check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
+    end
+
+  end
 end
