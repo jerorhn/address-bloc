afdf93199cbaaa1ac8d42056b9a1693f2f75fe4e
Hannah Jeror <jerorhn@yahoo.com>
2018-03-01 13:08:13 -0500

    Create option to remove entries

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -31,4 +31,40 @@ RSpec.describe AddressBook do
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
     end
   end
+
+  describe "#remove_entry" do
+    it "removes only one entry to the address book" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Beth Lovelace', '010.483.1815', 'beth.king@lovelace.com')
+      book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expect(book.entries.size).to eq(1)
+    end
+    it "removes the correct information from entries when the first entry is removed" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Beth Lovelace', '010.483.1815', 'beth.king@lovelace.com')
+      book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+
+      expect(book.entries[0].name).to eq('Beth Lovelace')
+      expect(book.entries[0].phone_number).to eq('010.483.1815')
+      expect(book.entries[0].email).to eq('beth.king@lovelace.com')
+    end
+    it "removes the correct information from entries when a later entry is removed" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Beth Lovelace', '010.483.1815', 'beth.king@lovelace.com')
+      book.add_entry('Dolly Lovelace', '483.483.1815', 'dolly.king@lovelace.com')
+      book.remove_entry('Beth Lovelace', '010.483.1815', 'beth.king@lovelace.com')
+
+      expect(book.entries[0].name).to eq('Ada Lovelace')
+      expect(book.entries[0].phone_number).to eq('010.012.1815')
+      expect(book.entries[0].email).to eq('augusta.king@lovelace.com')
+
+      expect(book.entries[1].name).to eq('Dolly Lovelace')
+      expect(book.entries[1].phone_number).to eq('483.483.1815')
+      expect(book.entries[1].email).to eq('dolly.king@lovelace.com')
+    end
+  end
+
 end
